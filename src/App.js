f90bfacbdd7a0de118a0ae29329d28221aa92d0b
unknown <simeonolawale69@gmail.com>
2022-05-16 17:52:11 +0100

    ...

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       {/* <Home/> */}
-      <BrowserRouter basename="/MyProfile">
+      <BrowserRouter>
         <Routes>
           <Route path="/home" element={<Home />} />
           <Route element={<ProtectedRoutes />}>
